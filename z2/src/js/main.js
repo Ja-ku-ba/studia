286c42d541f0db0c6f242524241772cc9a254538
hp-windows <wulfrykpierozek@gmail.com>
2023-07-14 19:15:45 +0200

    z2 landspace is done

--- z2/src/js/main.js
+++ z2/src/js/main.js
@@ -1,13 +1,20 @@
-const menuElement = document.getElementById('menu');
 const menuToggleElement = document.getElementById('toggle');
 
 menuToggleElement.addEventListener('click', function() {
     menuElement.classList.toggle('visible');
 });
 
-const lanscapeBreakpoint = window.matchMedia('(min-width: 720px)');
+const menuElement = document.getElementById('menu');
+const lanscapeBreakpoint = window.matchMedia('(min-width: 721px)');
+const accountLink = document.querySelectorAll('.account-link');
 lanscapeBreakpoint.addEventListener('change', function() {
     if (lanscapeBreakpoint.matches) {
         menuElement.classList.remove('visible');
+        accountLink[1].classList.add('hidden');
+        accountLink[0].classList.remove('hidden');
+    }
+    else {
+        accountLink[1].classList.remove('hidden');
+        accountLink[0].classList.add('hidden');
     }
 });
