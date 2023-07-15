c9cc93ad98a8ab31fbc577dcb223f29b8d3b1de3
hp-windows <wulfrykpierozek@gmail.com>
2023-07-15 14:27:07 +0400

    readme chcek

--- z1/src/js/main.js
+++ z1/src/js/main.js
@@ -2,13 +2,11 @@ const resposiveElements = document.querySelectorAll('.responsive');
 const lanscapeBreakpoint = window.matchMedia('(min-width: 721px)');
 lanscapeBreakpoint.addEventListener('change', function() {
     if (lanscapeBreakpoint.matches) {
-        document.body.classList.add('landscape')
         resposiveElements.forEach(function(element) {
             element.classList.add('landscape');
           });
     }
     else {
-        document.body.classList.remove('landscape')
         resposiveElements.forEach(function(element) {
             element.classList.remove('landscape');
           });
