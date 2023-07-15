19f90cbf833ae08c59d9c6a06dcb932717923d7a
hp-windows <wulfrykpierozek@gmail.com>
2023-07-15 15:55:13 +0400

    z2 is done

---
+++ z2/README.md
@@ -0,0 +1,19 @@
+# Poldroid
+
+## To launch the app:
+1. git clone git@github.com:Ja-ku-ba/studia.git
+2. windows: cd .\studia\z2\
+3. npm install
+4. npm run dev
+
+### To properly display the content:
+1. Open the port on which the application is running
+2. Then select the mode: "inspect"
+3. Foolow the images
+a. select button from image, or press ctr + shift + m (on chrome) 
+ ![Zdjecie krok 1](readme_files/krok1.png)
+b. set dimensions 720x1280. Why? The Internet said so
+ ![Zdjecie krok 2](readme_files/krok2.png)
+c. change screen orientation
+ ![Zdjecie krok 3](readme_files/krok3.png)
+4. close the "inspec" mode, and check the desktop style
--- z2/src/js/main.js
+++ z2/src/js/main.js
@@ -1,12 +1,12 @@
 const menuToggleElement = document.getElementById('toggle');
+const menuElement = document.getElementById('menu');
+const accountLink = document.querySelectorAll('.account-link');
 
 menuToggleElement.addEventListener('click', function() {
     menuElement.classList.toggle('visible');
 });
 
-const menuElement = document.getElementById('menu');
 const lanscapeBreakpoint = window.matchMedia('(min-width: 721px)');
-const accountLink = document.querySelectorAll('.account-link');
 lanscapeBreakpoint.addEventListener('change', function() {
     if (lanscapeBreakpoint.matches) {
         menuElement.classList.remove('visible');
@@ -16,5 +16,30 @@ lanscapeBreakpoint.addEventListener('change', function() {
     else {
         accountLink[1].classList.remove('hidden');
         accountLink[0].classList.add('hidden');
+    }
+    
+    if (laptopBreakpoint.matches) {
+        console.log("zaskoczyło")
+        menuToggleElement.classList.add('laptop')
+        menuElement.classList.add('laptop')
+        accountLink[1].classList.remove('hidden');
+        accountLink[0].classList.add('hidden');
+    }
+    else {
+        menuToggleElement.classList.remove('laptop')
+        menuElement.classList.remove('laptop')
+    }
+});
+
+const laptopBreakpoint = window.matchMedia('(min-width: 1281px)');
+laptopBreakpoint.addEventListener('change', function() {
+    if (laptopBreakpoint.matches) {
+        console.log("zaskoczyło")
+        menuToggleElement.classList.add('laptop')
+        menuElement.classList.add('laptop')
+    }
+    else {
+        menuToggleElement.classList.remove('laptop')
+        menuElement.classList.remove('laptop')
     }
 });
